2ca5c0053ec461b4484f893eb1072b2fcc896385
Khyroa <160810261+Khyroa@users.noreply.github.com>
2026-04-22 23:18:37 +0800

    Delete test26.py

--- test26.py
+++
@@ -1,6 +0,0 @@
-weight = float(input("Enter your weight in kilograms: "))
-height = float(input("Enter your height in meters: "))
-
-bmi = weight / (height * height)
-
-print(f"Your Body Mass Index (BMI) is {bmi:.2f}")
