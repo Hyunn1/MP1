8b257d4bdb586fcd050b9f98533607f21d5da6bd
Hyunn1 <167040368+Hyunn1@users.noreply.github.com>
2026-04-24 15:54:06 +0800

    Rename test26.py to exercise26.py

---
+++ exercise26.py
@@ -0,0 +1,5 @@
+import time
+
+current_time = time.asctime()
+
+print(f"The current date and time is: {current_time}")
--- test26.py
+++
@@ -1,5 +0,0 @@
-import time
-
-current_time = time.asctime()
-
-print(f"The current date and time is: {current_time}")
